3acc8e518d668b90f026a9639b88083493a33738
justinj.qld <justinj.qld@fea29735-8b0a-fe32-dc9e-1301376cf70f>
2010-08-15 12:33:40 +0000

    worked a bit more in main

--- carscrossing/src/carscrossing/Lane.java
+++ carscrossing/src/carscrossing/Lane.java
@@ -1,8 +1,15 @@
+
+package carscrossing;
+
+
+
+import java.util.Scanner;
+
+
+
 /*
  *Lane object class
  */
-
-package carscrossing;
 
 /**
  *
@@ -10,8 +17,69 @@ package carscrossing;
  */
 public class Lane {
     //Lane needs definative direction for future builds.
-String Direction;
 
-int length;
-boolean carMax;
+    String Direction;
+    int length;
+    boolean carMax;
+
+    public static void main(String[] args) {
+        // create user input variables
+
+        String[] inputArray = new String[4];
+        inputArray[0]="South to North: ";
+        inputArray[1]="North to South: ";
+        inputArray[2]="West to East: ";
+        inputArray[3]="East to West: ";
+        int LanesSN;
+        int LanesNS;
+        int LanesWE;
+        int LanesEW;
+
+        for (int k=0; k<4; ++k) {
+
+            Scanner keyboard = new Scanner(System.in);
+            // Ask user for input
+            System.out.println("Please enter the numer of lanes running"
+                + inputArray[k]);
+            if (k==0) {
+                LanesSN = keyboard.nextInt();
+            }
+            if (k==1) {
+                LanesNS = keyboard.nextInt();
+            }
+            if (k==2) {
+                LanesWE = keyboard.nextInt();
+            }
+            if (k==3) {
+                LanesEW = keyboard.nextInt();
+            }
+        }
+        // The reason for creating these variables is as individual
+        // lanes are created they will span accross other lanes,
+        // thus increasing their array length by the amount of lanes
+        // they cross.
+
+        int horizLanes  = (LanesWE + LanesEW);
+        int vertLanes  = (LanesSN + LanesNS);
+        int totalLanes  = (horizLanes + vertLanes);
+        int nL = 1;
+
+        for (int x=0; x<totalLanes ; ++x ){
+
+            if(LanesSN>0){
+                String i = String.
+            }
+
+
+
+
+        }
+
+
+
+
+
+
+
+    }
 }
