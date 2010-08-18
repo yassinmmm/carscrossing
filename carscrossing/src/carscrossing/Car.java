0092a764c56e13400cf1b3428df25c720c43571b
justinj.qld <justinj.qld@fea29735-8b0a-fe32-dc9e-1301376cf70f>
2010-08-18 06:24:45 +0000

    this is the edit done with assistance from Ashley during week 4 prac

--- carscrossing/src/carscrossing/Car.java
+++ carscrossing/src/carscrossing/Car.java
@@ -9,24 +9,6 @@ package carscrossing;
  * @author Pro
  */
 public class Car {
-    //Attribute for lane object to check in case lane
-    //before lights becomes full and causes problems
-boolean LaneMaxCar;
 
-//Getter method for LaneMaxCar attribute
-boolean getLaneMAxCar(){
-    return LaneMaxCar;
-}
-
-//Setter  method for LaneMaxCar attribute
-void setLaneMaxCar(boolean value){
-    LaneMaxCar = value;
-}
-
-
-//
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-
+public boolean exists = false;
 }
